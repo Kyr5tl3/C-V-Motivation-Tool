a3a97dd02b9040d2ed186a63cb69daeeca03ee5e
Kyrstle Breslin <kyrstle@live.com.au>
2018-11-12 23:26:23 +0000

    create release & centre button scripts

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 
 $('.wedge > img').css({'opacity':'1'});
+
+$('.release > button').click(function(){
+  console.log('release button clicked')
+  event.preventDefault();
+  $('.wedge > button').hide();
+  $('.release > .descriptions').show();
+  $('.centrecap > button').html('RESTART');
+  $('.centrecap > button').addClass('centrecap');
+})
+
+
+// Reset button headings
+$('.centrecap > button').click(function(){
+  $('.centrecap > button').html('CRAFT BRINGS THE SOUL BACK INTO BEER');
+  $('.centrecap > button').
+  $('button').show();
+  $('.descriptions').hide()
+  $('.centrecap > button').removeClass('centrecap');
+})
